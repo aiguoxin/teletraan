53c8dbf66a8d721d109b2ae62612f69491ed2047
艾国信 <aiguoxin@qiyi.com>
2016-04-14 20:38:16 +0800

    状态码数据动态补充，防止画图缺失

--- deploy-board/deploy_board/webapp/code_views.py
+++ deploy-board/deploy_board/webapp/code_views.py
@@ -50,30 +50,41 @@ def index_page(request):
     ip = request.POST.get('ip', '10.1.193.60')
     name = request.POST.get('name', None)
 
-    # 获取折线图数据
+    # 获取折线图数据,返回的数据中，不同状态码可能缺失数据，需要补全，当all为3个数据时，则无法区分[2,3]是代表[2,3,0]还是[2,0,3]
     xData = set()
+    xChangeData = set()
     proxyDict = {}
     params = [('ip', ip), ('name', name), ('after', date_to_timestamp(after)), ('before', date_to_timestamp(before))]
     if name is not None:
         get_interface_log = graph_helper.get_line_graph_data(request, params)
         if get_interface_log is not None:
+            # 获取all的时间
+            for i, model in enumerate(get_interface_log['all']):
+                xData.add(model['createTime'])
+                xChangeData.add(timestamp_to_date(model['createTime']))
+            # 获取具体折线所需y轴数据
             for key, val in get_interface_log.items():
                 proxyData = []
-                for i, el in enumerate(val):
-                    proxyData.append(el['codeCount'])
-                    xData.add(timestamp_to_date(el['createTime']))
+                for date in xData:
+                    findDate = 0
+                    for i, el in enumerate(val):
+                        if date == el['createTime']:
+                            proxyData.append(el['codeCount'])
+                            findDate = 1
+                    if findDate == 0:  # 补充数据
+                        proxyData.append(0)
                 proxyDict[key] = proxyData
-    xData = sorted(list(xData))
     proxyDict = json.dumps(proxyDict, encoding="UTF-8", ensure_ascii=False)
+    xData = json.dumps(sorted(list(xChangeData)), encoding="UTF-8", ensure_ascii=False)
 
-    # 获取饼图数据
+    # 获取阶段固定饼图数据
     pieDict = {}
     params = [('ip', ip), ('name', name), ('after', date_to_timestamp(after)), ('before', date_to_timestamp(before))]
     if name is not None:
         get_pie_data = graph_helper.get_pie_graph_data(request, params)
         pieDict = json.dumps(get_pie_data, encoding="UTF-8", ensure_ascii=False)
     return render(request, 'code/proxy.html',
-              {"after": after, "before": before, "name": name, "ip": ip, "xData": json.dumps(xData),
+              {"after": after, "before": before, "name": name, "ip": ip, "xData": xData,
                "pieDict": pieDict,
                "proxyDict": proxyDict,
                "csrf_token": get_token(request)})
